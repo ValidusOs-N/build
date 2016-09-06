DirtyUnicorns/android_build

Bring a few things inline with our lollipop branch
Change-Id: I8ece9573d6f5a5d801b26e1e71abc501ac541f60

core/product_config.mk
@@ -185,7 +185,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
   ifneq ($(DU_BUILD),)
-    all_product_configs := device/*/$(DU_BUILD)/du.mk
+    all_product_configs := $(shell ls device/*/$(DU_BUILD)/du.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
@@ -193,6 +193,7 @@ else
   endif # DU_BUILD
 endif
 
+ifeq ($(DU_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
@@ -214,6 +215,10 @@ $(foreach f, $(all_product_configs),\
 _cpm_words :=
 _cpm_word1 :=
 _cpm_word2 :=
+else
+    current_product_makefile := $(strip $(all_product_configs))
+    all_product_makefiles := $(strip $(all_product_configs))
+endif
 current_product_makefile := $(strip $(current_product_makefile))
 all_product_makefiles := $(strip $(all_product_makefiles))
 
@@ -249,7 +254,6 @@ endif
 current_product_makefile :=
 all_product_makefiles :=
 all_product_configs :=
-
 
 #############################################################################
 

envsetup.sh
@@ -76,6 +76,7 @@ function check_product()
     else
        DU_BUILD=
     fi
+    export DU_BUILD
 
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \

ValidusOs-N/build

core/product_config.mk
@@ -193,6 +193,7 @@ else
   endif # VALIDUS_BUILD
 endif
 
+ifeq ($(VALIDUS_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
@@ -214,6 +215,10 @@ $(foreach f, $(all_product_configs),\
 _cpm_words :=
 _cpm_word1 :=
 _cpm_word2 :=
+else
+    current_product_makefile := $(strip $(all_product_configs))
+    all_product_makefiles := $(strip $(all_product_configs))
+endif
 current_product_makefile := $(strip $(current_product_makefile))
 all_product_makefiles := $(strip $(all_product_makefiles))
 
@@ -261,7 +266,6 @@ endif
 current_product_makefile :=
 all_product_makefiles :=
 all_product_configs :=
-
 
 #############################################################################
 

envsetup.sh
@@ -135,6 +135,7 @@ function check_product()
     else
        VALIDUS_BUILD=
     fi
+    export VALIDUS_BUILD
 
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \